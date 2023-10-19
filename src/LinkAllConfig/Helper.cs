daaa419969ae71a552ef9675149dc5c74c690e78
Jimbo Tang <happycoder@foxmail.com>
2023-10-19 14:16:44 +0800

    `

--- src/LinkAllConfig/Helper.cs
+++ src/LinkAllConfig/Helper.cs
@@ -1,31 +1,42 @@
-﻿using CliWrap;
-using CliWrap.Buffered;
-using LinkAllConfig.Properties;
-using System.Diagnostics;
-using System.Text;
+﻿namespace LinkAllConfig;
 
-namespace LinkAllConfig;
-
-internal class Helper
+/// <summary>
+/// The helper.
+/// </summary>
+internal static class Helper
 {
   /// <summary>
   /// The target file path.
   /// </summary>
   private const string TargetFilePath = @"C:\Windows\System32\fsutil.exe";
 
+  /// <summary>
+  /// Lists the links.
+  /// </summary>
+  /// <param name="target">   The target. </param>
+  /// <param name="callback"> The callback. </param>
+  /// <returns> A Task. </returns>
   public static async Task<string> ListLinks(string target, Action<string> callback)
   {
-    StringBuilder result = new StringBuilder();
-    await Cli.Wrap(TargetFilePath)
+    var result = new StringBuilder();
+    _ = await Cli.Wrap(TargetFilePath)
     .WithArguments(args => args.Add("hardlink").Add("list").Add(target))
     .WithStandardOutputPipe(PipeTarget.ToStringBuilder(result))
     .ExecuteAsync();
     return await Task.FromResult(result.ToString());
   }
 
+  /// <summary>
+  /// Makes the file hard links.
+  /// </summary>
+  /// <param name="links">  The links. </param>
+  /// <param name="target"> The target. </param>
+  /// <param name="result"> The result. </param>
+  /// <returns> A Task. </returns>
   public static async Task MakeFileHardLinks(IEnumerable<string> links, string target, StringBuilder result)
   {
     var outpipe = PipeTarget.ToStringBuilder(result, Encoding.UTF8);
+
     foreach (var link in links)
     {
       if (File.Exists(link))
@@ -35,17 +46,18 @@ internal class Helper
       try
       {
         var cmd = Cli.Wrap(TargetFilePath)
-         .WithArguments(args => args.Add("hardlink").Add("create").Add(link).Add(target))
-         .WithStandardOutputPipe(outpipe)
-       .WithStandardErrorPipe(outpipe)
-       // .WithValidation(CommandResultValidation.None)
-       ;
-        await cmd.ExecuteBufferedAsync();
-        result.AppendLine();
+            .WithArguments(args => args.Add("hardlink").Add("create").Add(link).Add(target))
+            .WithStandardOutputPipe(outpipe)
+            .WithStandardErrorPipe(outpipe)
+
+    // .WithValidation(CommandResultValidation.None)
+    ;
+        _ = await cmd.ExecuteBufferedAsync();
+        _ = result.AppendLine();
       }
       catch (Exception ex)
       {
-        result.AppendLine(ex.ToString());
+        _ = result.AppendLine(ex.ToString());
       }
     }
   }
